5583669488f0a67778146d514c473a9603a0234f
Abhi251159 <patelabhi10699@gmail.com>
2019-08-28 21:16:06 +0530

    added a tileset

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,5 +1,6 @@
 import {Scene} from 'phaser';
-import Player from './Player';
+import Player from './GameObjects/Player';
+import Platfoem from './GameObjects/Platforms';
 
 export default class GameScene extends Scene{
   constructor(){
@@ -8,6 +9,18 @@ export default class GameScene extends Scene{
 
   create(){
     this.player = new Player(this,100,100);
+    this.cameras.main.setBackgroundColor('#5d5d5d')
+    
+
+    this.map = this.add.tilemap('World',16,16);
+    
+    let tilemap = this.map.addTilesetImage('environment_set','tiles');
+    let platforms  = this.map.createStaticLayer('Base',tilemap).setScale(2);
+
+
+    this.physics.add.collider(this.player,platforms);
+    platforms.setCollisionByProperty({collides:true})
+
   }
   update(){
     this.player.update();
