7f0b4c9ae677b746b9654884757dd3da2f9f413a
Abhi251159 <patelabhi10699@gmail.com>
2019-08-29 21:28:29 +0530

    can now switch weapons..

--- src/scenes/GameObjects/Player/player.soldier.js
+++ src/scenes/GameObjects/Player/player.soldier.js
@@ -12,31 +12,51 @@ export default class Soldier extends Player{
     this.activeGun = null;
 
     // portal gun
-    let portalGun = new LaserGun(this.scene,this.x,this.y,this);
-    this.guns.add(portalGun);
-  
-  
+    // this.guns.add(portalGun);  
   }
 
   // TODO: when many guns will be added to this player
-  addGunsToGroup(){
-    
+  switchWeapon(){
+    // portal gun  
+    if(this.weaponKeys.Q.isDown){
+      let portalGun = new PortalGun(this.scene,this.x,this.y,this);      
+      this.setActiveGun(portalGun);
+    }
+    if(this.weaponKeys.E.isDown){
+      let laserGun = new LaserGun(this.scene,this.x,this.y,this);
+      this.setActiveGun(laserGun);
+    }
   }
 
   // TODO: will be added using a gui
-  changeActiveGun(gun){
-
+  setActiveGun(gun){
+    if(this.activeGun == null){
+      this.activeGun = gun;
+    }
+    else{
+      this.activeGun.setActive(false);
+      this.activeGun.setVisible(false);
+      this.activeGun = gun;
+    }
   }
 
   takeDamage(damage){
     this.health-= damage;
+    if(this.health <= 0){
+      this.anims.play('pmain-die');
+    }
   }
   
   update(){
     this.handleMovements();
+    this.switchWeapon();
     // update guns
-    this.guns.children.each(gun=>{
-      gun.update();
-    })
+    // this.guns.children.each(gun=>{
+    //   gun.update();
+    // })
+
+    if(this.activeGun != null){
+      this.activeGun.update();
+    }
   }
 }
