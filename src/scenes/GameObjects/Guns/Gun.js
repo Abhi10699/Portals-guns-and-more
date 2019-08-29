78afeafa3d19deb1be1b562495a53457124dd6fe
Abhi251159 <patelabhi10699@gmail.com>
2019-08-29 13:28:39 +0530

    Gun base class

---
+++ src/scenes/GameObjects/Guns/Gun.js
@@ -0,0 +1,37 @@
+import {GameObjects} from 'phaser';
+
+const {Sprite} = GameObjects;
+
+export default class Gun extends Sprite{
+  constructor(scene,x,y,key,frame){
+    super(scene,x,y,key,frame);
+    this.scene.add.existing(this); 
+
+    // configs
+    this.setScale(1.2);
+    this.positionOffset ={x:7,y:9};
+  
+  }
+
+  handlePositions(parent){
+    this.flipX = parent.flipX;
+
+    if(this.flipX == true){
+      this.positionOffset.x = -5;
+    }
+    else{
+      this.positionOffset.x = 5;
+    }
+    this.setPosition(parent.x + this.positionOffset.x,parent.y + this.positionOffset.y);
+  }
+
+  handleActions(){    
+    if(this.scene.input.activePointer.isDown){
+      this.shoot();
+    }
+  }
+
+  shoot(){
+    console.log("Actions coming soon");
+  }
+}
