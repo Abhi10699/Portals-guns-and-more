7d969ba5b11c327d580736849d09494a5afcde25
Abhi251159 <patelabhi10699@gmail.com>
2019-08-29 17:20:19 +0530

    Laser Gun Implementation

---
+++ src/scenes/GameObjects/Guns/Laser/Gun.laser.js
@@ -0,0 +1,40 @@
+import Gun from "../Gun";
+import LaserBullet from "./Laser";
+
+export default class LaserGun extends Gun{
+  constructor(scene,x,y,player){
+    super(scene,x,y,'guns',0);
+    this.player = player;
+    this.bulletGroup = this.scene.physics.add.group();
+    
+    this.fireRate = 100; // bullets per second
+    this.magazineSize = 30;
+
+    this.canShoot = true;
+  }
+
+  shoot(){
+    if(this.canShoot){
+      let _laserBullet = new LaserBullet(this);
+      this.bulletGroup.add(_laserBullet);
+      this.canShoot = false;
+      this.setCooldown();
+    }
+  }
+
+  setCooldown(){
+    this.scene.time.addEvent({
+      delay:this.fireRate,
+      callback:()=>{this.canShoot = true}
+    })
+  }
+
+  update(){
+    this.handleActions();
+    this.handlePositions(this.player);
+
+    this.bulletGroup.children.each(bullet=>{
+      bullet.fire();
+    })
+  }
+}
